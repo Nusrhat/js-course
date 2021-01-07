0fbe09731076030eff0cebeae9427735400c0196
Nusrhat <77073345+Nusrhat@users.noreply.github.com>
2021-01-07 22:05:29 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,57 @@
+//----------------- Task 1 ---------------------------
+
+   //   let inch = 60;
+  //    let feet = inch / 12;
+  //    console.log(feet);
+
+ // ----------------- Task 2 ------------------------
+
+       //     let feet = 10678;
+       //     function feetToMile () {
+      //      let result = feet / 5280;
+      //      console.log(result);
+      //        }
+
+
+ //----------------- Task 3 -------------------------
+
+          // function result (chair, table, bed) {
+        //     console.log(chair+(table*3)+(bed*5));
+      // }
+    // result(5, 2, 1);
+
+// -------------------------- Task 4 --------------------
+
+                      // function breakCalculator(n) {
+                          
+                      //     if ( n >= 1) {
+                      //         if (10 >= n) {
+                      //         console.log((n*1000)*15);
+                      //         } else if (20 >= n) {
+                      //         console.log(150000+(((n-10)*1000)*12));
+                      //         } else if (25 >= n) {
+                      //         console.log(150000+120000+(n-20)*1000*10);
+                      //         } 
+                      //     } else {
+                      //         console.log("Enter number");
+                      //     }
+                      // }
+
+// -------------------------- Task 5 ----------------------------------
+
+              //  let myFriends = ["Abul", "Rasel", "Rim"];
+
+               // for (let i = 0; i<myFriends.length; i++){
+               //   console.log(myFriends);
+               // }
+
+
+
+
+
+
+
+
+
+    
+  
